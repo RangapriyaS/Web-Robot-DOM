a7e3fa0d03ed8b002e884b1007c9a8b7f3a2e8c2
Rangapriya <rangapriyas@gmail.com>
2022-08-29 13:29:56 +0530

    hide/show of output elements

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,17 +1,56 @@
 console.log('Hi I am from main.js');
-alert('Click Start to begin');
+//alert('Click Start to begin');
+function initialise(){
+    console.log('I am inside initialise');
+    // Donot show the input and output fields of the html on load of the page.
+    document.querySelector('.output').style.display='none';
+   // document.querySelector('.input').style.display='none';
+    const inputElement = document.querySelector('.input');
+    if(inputElement == null)
+    {
+        console.log('Element not found');
+
+    }
+    else{
+        console.log(inputElement);
+        inputElement.style.display='none';
+    }
+}
 function onButtonClick(){
 //console.log('Hello! I am your directing Robot');
-alert('Hello! I am your directing Robot');
-const userResponse=prompt('Are you ready for instructions? Yes or No');
-console.log('UserInput:',userResponse);
-if(userResponse === "Yes"){
- alert('Ok we will begin now');
+const outputElement = document.querySelector('.output');
+outputElement.style.display='block';
+console.log('outputElement:',outputElement);
+if(outputElement == null){
+  console.log('Element not found');
 }
 else{
- alert('Ok i will wait'); 
+    outputElement.style.display='block';
+    console.log('Inside else loop');
+    //Creating element for asking questions, replacing a prompt element
+    //const userResponse=prompt('Are you ready for instructions? Yes or No');
+    const userQuestion = document.createElement('p');
+    userQuestion.textContent = 'Are you ready for instructions? Yes or No'
+    //console.log('UserInput:',userResponse);
+    const parentElement=document.querySelector('.output');
+    parentElement.appendChild(userQuestion);
+    userQuestion.style.color='green';
+    const inputElement = document.querySelector('.input');
+    inputElement.style.display='block';
+    console.log('inputElement');
+    //const userResponse = document.querySelector('.input');
+    if(inputElement === "Yes"){
+        alert('Ok we will begin now');
+        const userNotify = document.createElement('p'); 
+        userNotify.textContent='Please click Ready to go button';
+        userQuestion.appendChild(userNotify);
+       }
+       else{
+        alert('Ok i will wait'); 
+       }
 }
-alert('Please click Ready to go button');
+//alert('Hello! I am your directing Robot');
+
 }
 function selectRoom(){
 const userResponse=prompt('Which room to go now? Kitchen?Pooja?Kidsroom?Bedroom?');
